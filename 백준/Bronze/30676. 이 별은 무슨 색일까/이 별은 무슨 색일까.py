a285b7c96646e2b499e4fe51dd2d442d958586f3
leesanghun <vitash1215@gmail.com>
2024-08-16 23:47:14 +0900

    [Bronze V] Title: 이 별은 무슨 색일까, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/30676. 이 별은 무슨 색일까/이 별은 무슨 색일까.py
@@ -0,0 +1,16 @@
+Lambda = int(input())
+
+if Lambda >= 620:
+    print("Red")
+elif Lambda >= 590:
+    print("Orange")
+elif Lambda >= 570:
+    print("Yellow")
+elif Lambda >= 495:
+    print("Green")
+elif Lambda >= 450:
+    print("Blue")
+elif Lambda >= 425:
+    print("Indigo")
+elif Lambda >= 380:
+    print("Violet")
