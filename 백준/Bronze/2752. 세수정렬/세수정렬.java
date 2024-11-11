d7c07500e3d7e0d9419010a3a21aaacfa41c7c40
leesanghun <vitash1215@gmail.com>
2024-11-12 01:48:38 +0900

    [Bronze IV] Title: 세수정렬, Time: 108 ms, Memory: 14540 KB -BaekjoonHub

--- 백준/Bronze/2752. 세수정렬/세수정렬.java
+++ 백준/Bronze/2752. 세수정렬/세수정렬.java
@@ -2,17 +2,17 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.List;
-import java.util.stream.Collectors;
 
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        List<Integer> numbers = List.of(reader.readLine().split(" ")).stream().map(Integer::parseInt).collect(Collectors.toList());
+        List<String> numbers = List.of(reader.readLine().split(" "));
 
-        numbers.sort(Integer::compareTo);
-
-        numbers.forEach(num -> System.out.print(num + " "));
+        numbers.stream()
+                .map(Integer::parseInt)
+                .sorted(Integer::compareTo)
+                .forEach(num -> System.out.print(num + " "));
     }
 }
 
