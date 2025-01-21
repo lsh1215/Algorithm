e08051957a0d85a176198cccf7cccb5516a83110
leesanghun <vitash1215@gmail.com>
2025-01-21 14:31:41 -0800

    Title: k번째로 신기한 문자열, Time: 147ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 250122/k번째로 신기한 문자열/kth-special-string.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt(); 
+        int k = sc.nextInt();  
+
+        String T = sc.next();  
+
+        List<String> strings = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            String currentString = sc.nextLine();
+            if (currentString.startsWith(T)) {
+                strings.add(currentString);
+            }
+        }
+
+        // Sort the matching strings
+        Collections.sort(strings);
+
+        // Check if we have at least k matching strings
+        System.out.println(strings.get(k - 1));
+    }
+
+}
