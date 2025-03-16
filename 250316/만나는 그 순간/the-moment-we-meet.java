3d01efc50cc07d48c7879b6174d2f3703322a135
leesanghun <vitash1215@gmail.com>
2025-03-16 16:18:14 +0900

    Title: 만나는 그 순간, Time: 166ms, Memory: 12MB, Status: Runtime Error - CodeTree

---
+++ 250316/만나는 그 순간/the-moment-we-meet.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static final int MAX_T = 100000;
+    public static int[] posA = new int[MAX_T+1];
+    public static int[] posB = new int[MAX_T+1];
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        
+      // A가 매 초마다 서있는 위치를 기록
+        int timeA = 1;
+        for(int i = 0; i < n; i++) {
+            char d = sc.next().charAt(0); 
+            int t = sc.nextInt();
+            
+            while(t-- > 0) {
+                if(d == 'R')
+                    posA[timeA] = posA[timeA - 1] + 1;
+                else
+                    posA[timeA] = posA[timeA - 1] - 1;
+                
+                timeA++;
+            }
+        }
+        
+        // B가 매 초마다 서있는 위치를 기록
+        int timeB = 1;
+        for(int i = 0; i < m; i++) {
+            char d = sc.next().charAt(0); 
+            int t = sc.nextInt();
+            
+            while(t-- > 0) {
+                if(d == 'R')
+                    posB[timeB] = posB[timeB - 1] + 1;
+                else
+                    posB[timeB] = posB[timeB - 1] - 1;
+                
+                timeB++;
+            }
+        }
+        
+        int meetTime = -1;
+
+        for(int i=1; i<timeA; i++){
+            if(posA[i] == posB[i]){
+                meetTime = i;
+                break;
+            }
+        }
+
+        System.out.print(meetTime);
+    }
+}
